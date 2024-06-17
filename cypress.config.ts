bca7e25cdf1e26cfb64aedc5f6edcdf418ad3a90
Santhosh <santhoshshetty58@gmail.com>
2024-06-17 14:43:09 +0200

    allure-reporting, lint-staged and dependabot changes

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "cypress";
 import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
 import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+import allurewriter from "@shelex/cypress-allure-plugin/writer";
 
 async function setupNodeEvents(
   on: Cypress.PluginEvents,
   config: Cypress.PluginConfigOptions,
 ): Promise<Cypress.PluginConfigOptions> {
   await addCucumberPreprocessorPlugin(on, config);
+  allurewriter(on, config);
   on(
     "file:preprocessor",
     createBundler({ plugins: [createEsbuildPlugin(config)] }),
